18f3f54e69548732f5abcd17ef323ec3983c2202
sean-kintone <seanluse@cybozu.onmicrosoft.com>
2023-08-10 15:32:25 +0900

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv'
+dotenv.config();
+const PORT = 5000;
+const app = express();
+app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000'
+};
+const multipleRecordsEndpoint = 'https://techpro1.cybozu.com/k/v1/records.json';
+app.get('/getData', cors(corsOptions), async (req,
+  res) => {
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'X-Cybozu-API-Token': process.env.API_TOKEN
+    }
+  }
+  const parameters = '?app=308';
+  const response = await
+    fetch(multipleRecordsEndpoint + parameters,
+      fetchOptions);
+  const jsonResponse = await response.json();
+  res.json(jsonResponse);
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
