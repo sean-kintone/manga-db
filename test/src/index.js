13e4e07ace7b0ecf55addc1f9558c0e2bbe4c526
sean-kintone <seanluse@cybozu.onmicrosoft.com>
2023-08-10 15:43:16 +0900

    hmm

---
+++ test/src/index.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+const kintoneRecords = 'http://localhost:5000/getData';
+
+const callRestApi = async () => {
+  const response = await fetch(kintoneRecords);
+  const jsonResponse = await response.json();
+  console.log(jsonResponse);
+  const arrayOfLists = jsonResponse.records.map(
+    record => <li key={record.recordID.value} >{record.title.value} ({record.author.value})</li>
+  )
+  return arrayOfLists;
+};
+function RenderResult() {
+  const [apiResponse, setApiResponse] = useState('***now loading ***');
+
+  useEffect(() => {
+    callRestApi().then(
+      result => setApiResponse(result));
+  }, []);
+  return (
+    <div>
+      <h1>React App</h1>
+      <p>{apiResponse}</p>
+    </div>
+  );
+};
+ReactDOM.render(
+  <RenderResult />,
+  document.querySelector('#root')
+);
